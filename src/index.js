9101115773b9b9abc2f0f8eed75aa0c176412370
MsMLyons <ms.markilyons@gmail.com>
2023-03-20 06:09:22 -0400

    Add comments

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // main component to be rendered
+// section component to render the article component, according to the UI
 const BookList = () => {
     return (
         <section>
@@ -14,6 +15,7 @@ const BookList = () => {
 };
 
 // nested components
+// article component to render specific data to the section component
 const Book = () => {
     return (
         <article>
@@ -25,6 +27,8 @@ const Book = () => {
 };
 
 // additional components created to be nested in another component
+// implicit return (Image component) and explicit return (Title component) examples
+// implicit returns return a single expression, while explicit returns return more complex expressions and operations
 const Image = () => <h2>Nara's picture (jk - this is a placeholder for the image)</h2>;
 const Title = () => {
     return (<h2>This is a book title about Nara!</h2>)
