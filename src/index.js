200b8b8f2e95de9bd20e4c6ef91cfc30e6ec7f1c
MsMLyons <ms.markilyons@gmail.com>
2023-03-20 16:34:42 -0400

    Refactor Booklist component based on props

--- src/index.js
+++ src/index.js
@@ -4,21 +4,56 @@ import ReactDOM from "react-dom/client";
 import './index.css';
 
 
-// variables for Book component
-const author = 'Nathan Hunter';
-const title = 'The Art of Prompt Engineering with ChatGPT';
-const img = './images/book1.jpg';
+// objects to represent each book
+const firstbook = {
+    author: 'Nathan Hunter',
+    title:'The Art of Prompt Engineering with ChatGPT',
+    img: './images/book1.jpg',
+};
 
+const secondbook = {
+    author: 'Charles R. Severance',
+    title: 'Python for Everybody',
+    img: './images/book2.jpg',
+};
+
+const thirdbook = {
+    author: 'Brandeis Hill Marshall',
+    title: 'Data Conscience Algorithmic S1ege on Our Humanity',
+    img: './images/book3.jpg',
+};
+
+const fourthbook = {
+    author: 'Gayle Laakmann McDowell',
+    title: 'Crack the Coding Interview',
+    img: './images/book4.jpg',
+};
 
 // main component to be rendered
 // section component to render the nested article (Book) component
 const BookList = () => {
     return (
-        <section className="booklist">
-            <Book author={author} title={title} img={img} />
-            <Book author={author} title={title} img={img} />
-            <Book author={author} title={title} img={img} />
-            <Book author={author} title={title} img={img} />
+        <section className='booklist'>
+            <Book
+                author={firstbook.author}
+                title={firstbook.title}
+                img={firstbook.img}
+            />
+            <Book
+                author={secondbook.author}
+                title={secondbook.title}
+                img={secondbook.img}
+            />
+            <Book
+                author={thirdbook.author}
+                title={thirdbook.title}
+                img={thirdbook.img}
+            />
+            <Book
+                author={fourthbook.author}
+                title={fourthbook.title}
+                img={fourthbook.img}
+            />
         </section>
     );
 };
